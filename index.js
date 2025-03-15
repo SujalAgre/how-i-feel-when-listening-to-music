f04bce99bf2d02e8201f7b286703c15faa878894
unknown <sujalagre26@gmail.com>
2025-03-15 23:52:11 +0530

    add music info n back btn

--- index.js
+++ index.js
@@ -1,72 +1,136 @@
-const songs = [
-    {
-        name: 'alag-aasmaan',
-        artistName: 'Anuv Jain',
-        link: 'alag-aasmaan',
-    },
-    {
-        name: 'Do You Know, Do You Care?',
-        artistName: 'Phil Collins',
-        link: 'do-you-know',
-    }
-    ,
-    {
-        name: 'Tweak',
-        artistName: 'Duwap Kaine',
-        link: 'tweak',
-    }
-];
+import { songs } from './songs.js'
 
+console.log(songs)
+
+
+//random num gen between length of the array
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(songs.length)
+let rand = getRandomInt(0, songs.length - 1);
 
-const rand = getRandomInt(0, songs.length - 1);
-
-console.log(rand);
-
-const video = document.getElementById(`${songs[rand].link}`);
+let video;
 
 const clickToStart = document.querySelector('.click-to-start');
+const sortDown = document.querySelector('.fa-sort-down');
 const clickToStartLink = document.getElementById('click-to-start-link');
+const body = document.querySelector('body');
+let upArrowBtn;
 
 clickToStart.addEventListener('click', () => {
+
+    console.log('clicked');
+
+    body.insertAdjacentHTML('beforeend', `
+        <div class="video-container">
+            <a href="#front" id="link">
+                <i class="fa-solid fa-arrow-up"></i>
+            </a>
+            <i class="fa-solid fa-compact-disc">
+                <div class="music-info">
+                    <h6 class="artist-name"><a href="${songs[rand].artistUrl}" target="_blank" id="link">${songs[rand].artistName}</a></h6>
+                    <h2 class="song-name"><a href="${songs[rand].songUrl}" target="_blank" id="link">${songs[rand].name}</a></h2>
+                </div>
+            </i>
+            <video loop id="${songs[rand].link}" class="video">
+                <source src="${songs[rand].url}">
+            </video>
+        </div>
+    `);
+
+    video = document.getElementById(`${songs[rand].link}`);
+    upArrowBtn = document.querySelector('.fa-arrow-up');
+
+    upArrowBtn.addEventListener('click', () => {
+
+        let deleteEl = setInterval(() => {
+            const videoContainer = document.querySelector('.video-container');
+            videoContainer.remove();
+            clearInterval(deleteEl);
+
+            rand = getRandomInt(0, songs.length - 1);
+        }, 300)
+
+    })
 
     clickToStartLink.setAttribute('href', `#${songs[rand].link}`)
     video.play();
     fadeInAudio();
-
-
 })
+
+// sortDown.addEventListener('click', ()=>{
+//     console.log('clicked');
+
+//     body.insertAdjacentHTML('beforeend', `
+//         <div class="video-container">
+//             <a href="#front" id="link">
+//                 <i class="fa-solid fa-arrow-up"></i>
+//             </a>
+//             <a href="#${songs[rand].link}" id="link">
+//                 <i class="fa-solid fa-sort-down"></i>
+//             </a>
+//             <i class="fa-solid fa-compact-disc">
+//                 <div class="music-info">
+//                     <h6 class="artist-name"><a href="${songs[rand].artistUrl}" target="_blank" id="link">${songs[rand].artistName}</a></h6>
+//                     <h2 class="song-name"><a href="${songs[rand].songUrl}" target="_blank" id="link">${songs[rand].name}</a></h2>
+//                 </div>
+//             </i>
+
+//             <video id="${songs[rand].link}" class="video">
+//                 <source src="${songs[rand].url}">
+//             </video>
+//         </div>
+//     `);
+
+//     video = document.getElementById(`${songs[rand].link}`);
+//     upArrowBtn = document.querySelector('.fa-arrow-up');
+
+//     upArrowBtn.addEventListener('click', () => {
+
+//         let deleteEl = setInterval(() => {
+//             const videoContainer = document.querySelector('.video-container');
+//             videoContainer.remove();
+//             clearInterval(deleteEl);
+
+//             rand = getRandomInt(0, songs.length - 1);
+//         }, 300)
+
+//     })
+
+//     clickToStartLink.setAttribute('href', `#${songs[rand].link}`)
+//     video.play();
+//     fadeInAudio();
+// })
+
 
 function fadeInAudio() {
     video.volume = 0;
     video.play();
 
     let fade = setInterval(() => {
-        if (video.volume < 1) {
-            video.volume += 0.1; // Increase volume
+        if (video.volume + 0.1 >= 1) {
+            video.volume = 1;
+            clearInterval(fade);
         } else {
-            clearInterval(fade); // Stop when volume is 1
+            video.volume += 0.1;
         }
-    }, 150); // Adjust speed of fade-in
+    }, 150);
 }
 
+
+//opening blur
 window.addEventListener("load", function () {
     const frontPage = document.getElementById('front');
 
     let blurValue = 70;
-
-    console.log(blurValue);
 
     let blur = setInterval(() => {
         if (blurValue > 0) {
             blurValue -= 1
             frontPage.style.filter = `blur(${blurValue}px)`
         } else {
-            clearInterval(blur); // Stop when volume is 1
+            clearInterval(blur);
         }
     }, 50);
 });
---
+++ songs.js
@@ -0,0 +1,43 @@
+export const songs = [
+    {
+        name: 'Alag Aasmaan',
+        artistName: 'Anuv Jain',
+        link: 'alag-aasmaan',
+        url: 'https://files.catbox.moe/zqnca1.mp4',
+        artistUrl: 'https://open.spotify.com/artist/4gdMJYnopf2nEUcanAwstx?si=y-4tmA9HTEma-cmOxVoyQg',
+        songUrl: 'https://open.spotify.com/track/3bQsp4Vr9Rg4fNCx6HPOgX?si=4b8355afe0074518'
+    },
+    {
+        name: 'Do You Know, Do You Care?',
+        artistName: 'Phil Collins',
+        link: 'do-you-know',
+        url: 'https://files.catbox.moe/s7tcod.mp4',
+        artistUrl: 'https://open.spotify.com/artist/4lxfqrEsLX6N1N4OCSkILp?si=eDk2NyvURLWTLAe4rqaYVA',
+        songUrl: 'https://open.spotify.com/track/4zrqubnrXjgUdQOeUKI91X?si=31c1bde5e3044a5b'
+    },
+    {
+        name: 'Tweak',
+        artistName: 'Duwap Kaine',
+        link: 'tweak',
+        url: 'https://files.catbox.moe/or2qov.mp4',
+        artistUrl: 'https://open.spotify.com/artist/5fBcVfTWenqUDOYR5SjzEQ?si=K0a76LDHRfq4f3pInmyUgw',
+        songUrl: 'https://open.spotify.com/track/7fev0PTM3nADPEXJik73Dx?si=5add24e72d4b45f0'
+    },
+    {
+        name: 'Onnat!',
+        artistName: 'Diorvsyou, Yung Fazo',
+        link: 'onnat',
+        url: 'https://files.catbox.moe/q7bnah.mp4',
+        artistUrl: 'https://open.spotify.com/artist/05ATu9kvlippz8fNxGGQWy?si=68Hp5MhuQTmTi58k_rWozw',
+        songUrl: 'https://open.spotify.com/track/5MwvthWby4ft93b5ApcIw5?si=9b1c6fa258534cd3'
+    },
+    {
+        name: 'Movie Scene',
+        artistName: 'Aszu',
+        link: 'movie-scene',
+        url: 'https://files.catbox.moe/zuqlrc.mp4',
+        artistUrl: 'https://www.youtube.com/@okayAszu',
+        songUrl: 'https://www.youtube.com/watch?v=iiv2aKUJxPE'
+    }
+    
+];
